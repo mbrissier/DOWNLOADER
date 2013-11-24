d64108722d3f7d5071f2a65cf4c67a8800b75dd1
hans <hans@gmail.com>
2013-11-24 19:08:35 +0100

    project finish

--- src/com/example/downloader/DownloadService.java
+++ src/com/example/downloader/DownloadService.java
@@ -14,76 +14,74 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.ResultReceiver;
+import android.util.Log;
 import android.widget.Toast;
 
 public class DownloadService extends IntentService {
-    public 	static final int UPDATE_PROGRESS = 8344;
-    public 	static final String URL
-    	= "url";
-    public 	static final String RECEIVER
-		= "receiver";
-    private static final int	BUFFERSIZE
-		= 1024;
-    public static final String TOAST
-		= "Download abgeschlossen";
-    private static final String STORAGENOTWRITABLE
-		= "external storage is not accessable or not writable";
-    public DownloadService() {
-        super("DownloadService");
-    }
-    
-    /* Checks if external storage is available for read and write */
-    public boolean isExternalStorageWritable() {
-        String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            return true;
-        }
-        return false;
-    }
-    
-    @Override
-    protected void onHandleIntent(Intent intent) {
-        String	 		urlToDownload 	= intent.getStringExtra(URL);
-        ResultReceiver	receiver 		= (ResultReceiver) intent.getParcelableExtra(RECEIVER);
-        
-        try {
-            URL url = new URL(urlToDownload);
-            URLConnection connection = url.openConnection();
-            connection.connect();
-            // this will be useful so that you can show a typical 0-100% progress bar
-            int fileLength = connection.getContentLength();
+	public static final int UPDATE_PROGRESS = 8044;
+	public static final String URL = "url";
+	public static final String RECEIVER = "receiver";
+	public static final String DES = "/mnt/sdcard/test/test.iso";
+	private static final int BUFFERSIZE = 1024;
+	public static final String TOAST = "Download abgeschlossen";
+	
 
-            // download the file
-            InputStream input = new BufferedInputStream(url.openStream());
-            if(!isExternalStorageWritable())	throw new RuntimeException(STORAGENOTWRITABLE);
-            OutputStream output = new FileOutputStream
-            					 (new File(Environment.getExternalStoragePublicDirectory
-            					 (Environment.DIRECTORY_PICTURES), url.toString()));
+	public DownloadService() {
+		super("DownloadService");
+	}
 
-            byte data[] = new byte[BUFFERSIZE];
-            long total = 0;
-            int count;
-            while ((count = input.read(data)) != -1) {
-                total += count;
-                // publishing the progress....
-                Bundle resultData = new Bundle();
-                resultData.putInt(DownloadReceiver.PROGRESS ,(int) (total * DownloadReceiver.FINISHED / fileLength));
-                receiver.send(UPDATE_PROGRESS, resultData);
-                output.write(data, 0, count);
-            }
 
-            output.flush();
-            output.close();
-            input.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (RuntimeException e) {
-        	e.printStackTrace();
-        }
+	
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		String urlToDownload = intent.getStringExtra(URL);
+		ResultReceiver receiver = (ResultReceiver) intent
+				.getParcelableExtra(RECEIVER);
 
-        Bundle resultData = new Bundle();
-        resultData.putInt(DownloadReceiver.PROGRESS, DownloadReceiver.FINISHED);
-        Toast.makeText(this, TOAST, Toast.LENGTH_LONG).show();
-        receiver.send(UPDATE_PROGRESS, resultData);
-    }
+		try {
+			URL url = new URL(urlToDownload);
+			URLConnection connection = url.openConnection();
+			connection.connect();
+			// this will be useful so that you can show a typical 0-100%
+			// progress bar
+			int fileLength = connection.getContentLength();
+
+			// download the file
+			InputStream input = new BufferedInputStream(url.openStream());
+			// save file
+			OutputStream output = new FileOutputStream(DES);
+			
+			//get status of download and pass it to Downloader.progressBar via DownloadReceiver
+			byte data[] = new byte[BUFFERSIZE];
+			long total = 0;
+			int count;
+			while ((count = input.read(data)) != -1) {
+				total += count;
+				// publishing the progress....
+				Bundle resultData = new Bundle();
+				resultData.putInt(DownloadReceiver.PROGRESS, (int) (total
+						* DownloadReceiver.FINISHED / fileLength));
+				receiver.send(UPDATE_PROGRESS, resultData);
+				output.write(data, 0, count);
+			}
+
+			output.flush();
+			output.close();
+			input.close();
+		} catch (IOException e) {
+			Log.v("DownloadService", "Exception:"+e);
+		} catch (RuntimeException e) {
+			Log.v("DownloadService", "Exception:"+e);
+		}
+
+		Bundle resultData = new Bundle();
+		resultData.putInt(DownloadReceiver.PROGRESS, DownloadReceiver.FINISHED);
+		makeToast();
+		receiver.send(UPDATE_PROGRESS, resultData);
+	}
+	
+	public void makeToast() {
+		
+		Toast.makeText(this, TOAST, Toast.LENGTH_LONG).show();
+	}
 }
--- src/com/example/downloader/Downloader.java
+++ src/com/example/downloader/Downloader.java
@@ -1,73 +1,61 @@
 package com.example.downloader;
 
-import java.io.BufferedInputStream;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
+
 
 import android.os.Bundle;
 import android.os.Handler;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.Toast;
+
 
 public class Downloader extends Activity {
 
-	private Button 		button_download;
-	private ProgressBar progressBar_download;
-	private EditText 	editText_url;
-	
+	private Button button_download;
+	public ProgressBar progressBar_download;
+	private EditText editText_url;
+
 	protected static ProgressDialog progressBar;
-	
-	private static final String DEFAULTURL
-		= "https://www.ironmanmagazine.com/articles/6311-Watch-Arnold-Train.pdf";
-	
+
+	private static final String DEFAULTURL = "http://ftp.tu-clausthal.de/pub/mirror/ubuntu/releases//saucy/ubuntu-13.10-server-amd64.iso";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_downloader);
 
-		button_download 		= (Button) 		findViewById(R.id.button_download);
+		button_download = (Button) findViewById(R.id.button_download);
 		button_download.setOnClickListener(downloadListener);
-		progressBar_download 	= (ProgressBar) findViewById(R.id.progressBar_download);
-		editText_url 			= (EditText) 	findViewById(R.id.editText_url);
+		progressBar_download = (ProgressBar) findViewById(R.id.progressBar_download);
+		editText_url = (EditText) findViewById(R.id.editText_url);
 		editText_url.setText(DEFAULTURL);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.downloader, menu);
-		return true;
-
-	}
 	
+
 	OnClickListener downloadListener = new OnClickListener() {
 		public void onClick(View v) {
 			progressBar = new ProgressDialog(v.getContext());
 			progressBar.setCancelable(true);
-			progressBar.setMessage(getResources().getString(R.string.progressbar_message));
+			progressBar.setMessage(getResources().getString(
+					R.string.progressbar_message));
 			progressBar.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
 			progressBar.setProgress(0);
 			progressBar.setMax(DownloadReceiver.FINISHED);
 			progressBar.show();
 
 			Intent intent = new Intent(Downloader.this, DownloadService.class);
-			intent.putExtra(DownloadService.URL, editText_url.getText().toString() );
-			intent.putExtra(DownloadService.RECEIVER, new DownloadReceiver(new Handler()));
+			intent.putExtra(DownloadService.URL, editText_url.getText()
+					.toString());
+			intent.putExtra(DownloadService.RECEIVER, new DownloadReceiver(
+					new Handler()));
 			startService(intent);
 		}
 	};
 
-	
-	
 }
-
